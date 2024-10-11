a617c1f403aaa4b1fefcca86af7a72d2afb43241
krishm-htx <krish.htx@gmail.com>
2024-10-11 11:41:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "does this work? [docs.streamlit.io](https://docs.streamlit.io/)."
+    "does this work? [docs.streamlit.io](https://docs.streamlit.io/). Okay it does work!"
 )
